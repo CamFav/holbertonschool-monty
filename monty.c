9345cee1ea7ed360bb88dcf8940b4dff9f18bba8
Meashyshy <camillefvrl@gmail.com>
2023-05-09 06:00:49 -0700

    Added comments to gain time

--- monty.c
+++ monty.c
@@ -1,8 +1,86 @@
 #include "monty.h"
 
 /**
- * main - entry point
- * @
- * @
- * Return: 0
-*/
+ * push - adding element to stack
+ * @stack: linked list
+ * @line_number: line number
+ *
+ * Return: Nothing.
+ */
+void push(stack_t** stack, unsigned int line_number)
+{
+    /* Implementation of push opcode */
+    /* Add logic to push a new element onto the stack */
+}
+
+/**
+ * pall - print from the start node
+ * @stack: the header
+ * @line_number: for the error
+ */
+void pall(stack_t** stack, unsigned int line_number)
+{
+    /* Implementation of pall opcode */
+    /* Add logic to print all elements of the stack */
+}
+
+/**
+ * pint - print int in top of stack
+ * @stack: the header
+ * @line_number: still don't know
+ */
+void pint(stack_t** stack, unsigned int line_number)
+{
+    /* Implementation of pint opcode */
+    /* Add logic to print the value at the top of the stack */
+}
+
+/**
+ * pop - removes first element of stack
+ * @stack: linked list
+ * @line_number: line number
+ *
+ * Return: Nothing.
+ */
+void pop(stack_t** stack, unsigned int line_number)
+{
+    /* Implementation of pop opcode */
+    /* Add logic to remove the top element from the stack */
+}
+
+/**
+ * swap - swaps the top two elements of the stack.
+ * @stack: linked list
+ * @line_number: line number
+ *
+ * Return: Nothing.
+ */
+void swap(stack_t** stack, unsigned int line_number)
+{
+    /* Implementation of swap opcode */
+    /* Add logic to swap the top two elements of the stack */
+}
+
+/**
+ * add - additionne deux noeuds
+ * @stack: le header
+ * @line_number: la ligne où on est
+ */
+void add(stack_t** stack, unsigned int line_number)
+{
+    /* Implementation of add opcode */
+    /* Add logic to add the top two elements of the stack */
+}
+
+/**
+ * nop - desc
+ * @stack: linked list
+ * @line_number: line number
+ *
+ * Return: Nothing.
+ */
+void nop(stack_t** stack, unsigned int line_number)
+{
+    /* Implementation of nop opcode */
+    /* Add logic for the no operation (nop) opcode */
+}
