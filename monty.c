9e75ce8b788b6ce36bc96eca0f0c863a756462c1
Meashyshy <camillefvrl@gmail.com>
2023-05-11 05:06:10 -0700

    Added pop function

--- monty.c
+++ monty.c
@@ -85,3 +85,29 @@ void pint(stack_t **stack, unsigned int line_number)
     printf("%d", (*stack)->n);
     /* Print the value at the top of the stack */
 }
+
+/**
+ * pop - removes first element of stack
+ * @stack: linked list
+ * @line_number: line number
+ *
+ * Return: Nothing.
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+    stack_t *topNode = NULL;
+    /* Implementation of pop opcode */
+    if (!*stack)
+    {
+        fprintf(stderr, "L<line_number>: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    topNode = (*stack);
+
+    if (*stack)
+        (*stack)->prev = NULL;
+
+    *stack = (*stack)->next;
+    free(topNode);
+    /* Add logic to remove the top element from the stack */
+}
