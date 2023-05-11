be8988c605144f78997c948ad4385b621e7077a6
Meashyshy <camillefvrl@gmail.com>
2023-05-11 06:59:40 -0700

    Added basic add function

--- monty.c
+++ monty.c
@@ -142,3 +142,20 @@ void swap(stack_t **stack, unsigned int line_number)
 		return;
 	}
 }
+
+/**
+ * add - add the top two elements of the stack
+ * @stack: linked list
+ * @line_number: line number
+ * 
+ * Return: Nothing.
+*/
+void add(stack_t **stack, unsigned int line_number)
+{
+    if (*stack == NULL || ((*stack)->next) == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+		return;
+	}
+}
