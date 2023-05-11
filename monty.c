b332579f120c4e9233bdce355ec049d47749cb8e
Meashyshy <camillefvrl@gmail.com>
2023-05-11 06:52:12 -0700

    Implimented error gestion

--- monty.c
+++ monty.c
@@ -135,5 +135,10 @@ void nop(stack_t **stack, unsigned int line_number)
 */
 void swap(stack_t **stack, unsigned int line_number)
 {
-    
+    if (*stack == NULL || ((*stack)->next) == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+		return;
+	}
 }
