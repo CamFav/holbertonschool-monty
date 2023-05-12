15c9cc8a2bb9285343ad9fbd8c3afd6d521aaf7e
Axel Malka <axelmalka@outlook.fr>
2023-05-12 13:47:27 +0000

    add error if divide by 0

--- stack_func3.c
+++ stack_func3.c
@@ -32,6 +32,13 @@ void _div(stack_t **stack, unsigned int line_number)
 		return;
 	}
 
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		EXIT_STATUS = EXIT_FAILURE;
+		return;
+	}
+
 	(*stack)->next->n /= (*stack)->n;
 	pop(stack, line_number);
 }
@@ -64,6 +71,13 @@ void _mod(stack_t **stack, unsigned int line_number)
 	if (!stack || !(*stack) || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		EXIT_STATUS = EXIT_FAILURE;
+		return;
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
 		EXIT_STATUS = EXIT_FAILURE;
 		return;
 	}
